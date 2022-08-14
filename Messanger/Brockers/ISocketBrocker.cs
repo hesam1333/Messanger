a752e572c6f5b96f3cea51a0446b979e825186a1
hesam1333 <hesam.fardad22@gmail.com>
2022-08-14 21:01:25 +0430

    Add: broker Interfaces

---
+++ Messanger/Brockers/ISocketBrocker.cs
@@ -0,0 +1,13 @@
+﻿using Messenger.Domain;
+using System.Net.WebSockets;
+
+namespace Messenger.Brockers
+{
+    public interface ISocketBrocker
+    {
+        Task SendStringAsync(HubModel ToHub, string data, CancellationToken ct = default(CancellationToken));
+        ValueTask<string> ReceiveStringAsync(HubModel FromHub, CancellationToken ct = default(CancellationToken));
+        IEnumerable<string> GetHubsList(CancellationToken ct = default(CancellationToken));
+        IEnumerable<MessageModel> GetHubsMessages(string hubId, CancellationToken ct = default(CancellationToken));
+    }
+}
