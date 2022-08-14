30d4ad3069cf967d284a18dfa854d65780d237f6
hesam1333 <hesam.fardad22@gmail.com>
2022-08-14 21:02:18 +0430

    Add: serialization implementation

---
+++ Messanger/Brockers/SerializationBrocker.cs
@@ -0,0 +1,18 @@
+﻿
+using Newtonsoft.Json;
+
+namespace Messenger.Brockers
+{
+    public class SerializationBrocker : ISerializationBroker
+    {
+        public T Deserilize<T>(string obj)
+        {
+            return JsonConvert.DeserializeObject<T>(obj);
+        }
+
+        public string Serilize<T>(T obj)
+        {
+           return  JsonConvert.SerializeObject(obj);
+        }
+    }
+}
