db7cd97d9d1828500fd8dc410f7e6ea92ddf77b6
hesam1333 <hesam.fardad22@gmail.com>
2022-08-15 17:28:54 +0430

    Refactor: just clean up some code :)

--- AcceptanceTest/Program.ShowUserMessage.cs
+++ AcceptanceTest/Program.ShowUserMessage.cs
@@ -22,29 +22,6 @@ namespace AcceptanceTest
             var json = JsonConvert.SerializeObject(socketCommand);
             await SocketClient.SendAsync(json, ct);
 
-            //string respond = await SocketClient.ReceiveMessageAsync(ct);
-            //BaseMessaginModel resivedMessage = JsonConvert.DeserializeObject<BaseMessaginModel>(respond);
-
-            //if (resivedMessage.MessageType == 100)
-            //{
-            //    var Jbody = (JArray)resivedMessage.Body;
-            //    var body = Jbody.ToObject<List<MessageModel>>();
-
-            //    Console.WriteLine(String.Format("messages list for {0} : \n", userId));
-            //    foreach (var item in body)
-            //    {
-            //        Console.WriteLine(String.Format("sender Id : {0}  ", item.SenderId));
-            //        Console.WriteLine(String.Format("dateTime : {0}  ", item.CreateDateTime.ToString()));
-            //        Console.WriteLine(String.Format("message : {0}  \n\n", item.MessageBody));
-            //    }
-            //}
-            //else
-            //{
-            //    var jbody = (JObject)resivedMessage.Body;
-            //    var body = jbody.ToObject<MessageModel>();
-
-            //    Console.WriteLine(body.MessageBody);
-            //}
         }
 
     }
--- AcceptanceTest/Program.cs
+++ AcceptanceTest/Program.cs
@@ -61,51 +61,6 @@ namespace AcceptanceTest
                                 "\n ___________________________________");
         }
 
-        private static async void listenToRespond(CancellationToken ct)
-        {
-            while (true)
-            {
-                string respond = await SocketClient.ReceiveMessageAsync(ct);
-                BaseMessaginModel resivedMessage = JsonConvert.DeserializeObject<BaseMessaginModel>(respond);
-
-                if(resivedMessage.MessageType == 100)
-                    Console.WriteLine("\n\nRecived info::\n");
-                else
-                    Console.WriteLine("\n\nRecived error::\n");
-
-                if (resivedMessage.Body is JArray)
-                {
-                    var Jbody = (JArray)resivedMessage.Body;
-                    var body = Jbody.ToObject<List<object>>();
-
-                    foreach (var item in body)
-                    {
-                        if (item is string)
-                        {
-                            Console.WriteLine(String.Format("user Id :" + item.ToString() + "\n\n"));
-                        }
-                        else
-                        {
-                            var jbody = (JObject)item;
-                            var model = jbody.ToObject<MessageModel>();
-
-                            Console.WriteLine(String.Format("sender Id : {0}  ", model.SenderId));
-                            Console.WriteLine(String.Format("dateTime : {0}  ", model.CreateDateTime.ToString()));
-                            Console.WriteLine(String.Format("message : {0}  \n\n", model.MessageBody));
-                        }
-                    }
-                }
-                else
-                {
-                    var jbody = (JObject)resivedMessage.Body;
-                    var body = jbody.ToObject<MessageModel>();
-
-                    Console.WriteLine(body.MessageBody + "\n\n");
-                }
-
-            }
-        }
-
     }
 }
 
